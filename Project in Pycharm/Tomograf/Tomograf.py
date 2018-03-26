a9d0e7b3863fd8b781dcaa1351a06abf4e924409
Mateusz Kapiszewski <kapiszewski.mateusz@gmail.com>
2018-03-26 17:18:56 +0200

    Working version with slider, without filtering, without error handling

--- Project in Pycharm/Tomograf/Tomograf.py
+++ Project in Pycharm/Tomograf/Tomograf.py
@@ -3,6 +3,8 @@ import scipy
 import numpy as np
 import math
 import kivy
+import cv2
+from copy import deepcopy
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
@@ -16,6 +18,8 @@ class Controller(BoxLayout):
     def __init__(self):
         super(Controller, self).__init__()
         self.ids.file_chooser.rootpath = PATH
+        self.ids.slider.max = SLIDER_MAX
+        self.ids.slider.value = SLIDER_MAX
 
     def first_button_clicked(self):
         global image
@@ -26,23 +30,30 @@ class Controller(BoxLayout):
 
     def second_button_clicked(self):
         global image, sinogram, RANGE, STEPS, R, HALF_DETECTORS, WIDTH, HEIGHT, DETECTORS, STEP
-        STEP = math.pi / 90
-        RANGE = 0.2 * math.pi
+        RANGE = float(self.ids.detectors_range_input.text)*math.pi/180
         DETECTORS = int(self.ids.detectors_number_input.text)
-        STEPS = math.floor(math.pi / STEP)
+        STEPS = int(self.ids.steps_input.text)
+        STEP = math.pi / STEPS
         R = (len(image) ** 2 + len(image[0]) ** 2) ** 0.5
         HALF_DETECTORS = DETECTORS // 2
         WIDTH = len(image[0]) - 1
         HEIGHT = len(image) - 1
-        sinogram = build_sinogram(image)
-        misc.imsave("sinogram.jpg", sinogram)
-        self.ids.second_image.source = 'sinogram.jpg'
+        sinogram = build_sinogram(image, self)
+        for i in range(1,SLIDER_MAX+1):
+            misc.imsave("sinogram_{0}.jpg".format(i), sinogram[:int(STEPS*i/SLIDER_MAX)])
+        self.ids.second_image.source = 'sinogram_{0}.jpg'.format(SLIDER_MAX)
         self.ids.second_image.reload()
 
     def third_button_clicked(self):
         recoveredImage = recover_image(sinogram)
-        misc.imsave("recovered.jpg", recoveredImage)
-        self.ids.third_image.source = 'recovered.jpg'
+        self.ids.third_image.source = "recovered_image_{0}.jpg".format(SLIDER_MAX)
+        self.ids.third_image.reload()
+
+    def slider_touched_up(self):
+        value = self.ids.slider.value
+        self.ids.second_image.source = "sinogram_{0}.jpg".format(value)
+        self.ids.third_image.source = "recovered_image_{0}.jpg".format(value)
+        self.ids.second_image.reload()
         self.ids.third_image.reload()
 
 
@@ -51,18 +62,23 @@ class ActionApp(App):
         return Controller()
 
 
+
 class SinogramExecutor:
     result = 0
+    pixel_count = 0
     image = None
 
     def __init__(self, image):
         self.image = image
 
     def perform(self, y, x):
+        self.pixel_count += 1
         self.result += image[y][x]
 
     def return_result(self):
-        return int(self.result)
+        if self.pixel_count == 0:
+            return 0
+        return int(self.result / self.pixel_count)
 
 class RecoveryExecutor:
     value = 0
@@ -118,9 +134,9 @@ def compute_coordinates(phi):
     x = int(x + 0.5 * len(image[0]))
     return (y, x)
 
-def build_sinogram(image):
+def build_sinogram(image, self):
     sinogram = []
-    for i in range(STEPS):
+    for i in range(int(STEPS)): # * (self.ids.first_image_slider.value / 4))):
         middle_angle = i * STEP
         projection = []
         for ray in range(-HALF_DETECTORS, HALF_DETECTORS, 1):
@@ -129,25 +145,40 @@ def build_sinogram(image):
             end_point = compute_coordinates(middle_angle + math.pi - shift_angle)
             projection.append(bresenhamLine(start_point, end_point, SinogramExecutor(image)))
         sinogram.append(projection)
+        # if i % STEPS/15:
+        #     misc.imsave("sinogram.jpg", sinogram)
+        #     self.ids.second_image.source = 'sinogram.jpg'
+        #     self.ids.second_image.reload()
     return sinogram
 
 def recover_image(sinogram):
     recoveredImage = [[0 for x in range(len(image[0]))] for y in range(len(image))]
-    for i in range(STEPS):
-        middle_angle = i * STEP
-        for ray in range(-HALF_DETECTORS, HALF_DETECTORS, 1):
-            shift_angle = ray * RANGE / DETECTORS
-            start_point = compute_coordinates(middle_angle + shift_angle)
-            end_point = compute_coordinates(middle_angle + math.pi - shift_angle)
-            value = sinogram[i][ray + HALF_DETECTORS]
-            recoveredImage = bresenhamLine(start_point, end_point, RecoveryExecutor(recoveredImage, value))
+    for z in range(SLIDER_MAX):
+        CHUNK = STEPS // SLIDER_MAX
+        for i in range(z*CHUNK, (z+1) * CHUNK):
+            middle_angle = i * STEP
+            for ray in range(-HALF_DETECTORS, HALF_DETECTORS, 1):
+                shift_angle = ray * RANGE / DETECTORS
+                start_point = compute_coordinates(middle_angle + shift_angle)
+                end_point = compute_coordinates(middle_angle + math.pi - shift_angle)
+                value = sinogram[i][ray + HALF_DETECTORS]
+                recoveredImage = bresenhamLine(start_point, end_point, RecoveryExecutor(recoveredImage, value))
+
+        # filtered_image = None
+        # filtered_image = cv2.bilateralFilter(np.asarray(deepcopy(recoveredImage)), 15, 80, 80)
+        misc.imsave("recovered_image_{0}.jpg".format(z+1), recoveredImage)
     return recoveredImage
+
+
+def filtering(image):
+    #return image
+    return cv2.blur(np.asarray(image), (5, 5)) #cv2.filter2D(image, -1, kernel)
 
 if __name__ == '__main__':
 
     image = []
     sinogram = []
-    R, HALF_DETECTORS, WIDTH, HEIGHT, STEPS, STEP, RANGE, DETECTORS = 0, 0, 0, 0, 0, 0, 0, 0
+    R, HALF_DETECTORS, WIDTH, HEIGHT, STEPS, STEP, RANGE, DETECTORS, SLIDER_MAX = 0, 0, 0, 0, 0, 0, 0, 0, 10
     PATH = 'zdjecia'
     Window.fullscreen = True
     myApp = ActionApp()
